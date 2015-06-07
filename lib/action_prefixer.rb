410c1f66d6348b09e4f22d161b3c74c79eae4b3f
sinsoku <sinsoku.listy@gmail.com>
2015-06-07 15:28:43 +0900

    first implementation

--- lib/action_prefixer.rb
+++ lib/action_prefixer.rb
@@ -1,5 +1,23 @@
 require "action_prefixer/version"
 
 module ActionPrefixer
-  # Your code goes here...
+  module ActionPrefixes
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :expand_lookup_paths
+    end
+
+    def expand_lookup_paths
+      lookup_context.prefixes = ["#{controller_path}/#{action_name}"] + lookup_context.prefixes
+    end
+  end
+
+  class Railtie < ::Rails::Railtie
+    initializer 'action_prefixer' do
+      ActiveSupport.on_load(:action_controller) do
+        ActionController::Base.send :include, ActionPrefixes
+      end
+    end
+  end
 end
